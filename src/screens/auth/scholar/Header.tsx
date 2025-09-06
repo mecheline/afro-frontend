ed0371fb71a9e558dc073a1949396a1eac1d5b47
mecheline <ucheamaechi2016@gmail.com>
2025-09-06 13:05:38 +0100

    mobile login fix

--- src/screens/auth/scholar/Header.tsx
+++ src/screens/auth/scholar/Header.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../../../redux/services/scholar/store";
-import { Link } from "react-router";
+// import { Link } from "react-router";
 import { Bell } from "lucide-react";
 
 const Header = () => {
